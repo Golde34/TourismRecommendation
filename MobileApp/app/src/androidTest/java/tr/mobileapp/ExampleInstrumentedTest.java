d5f7946808eb9bb131649e194aef9753e433ee65
Golde34 <nguyendongducviet2001@gmail.com>
2022-09-27 00:32:32 +0700

    update mobile app
    update mobile app

--- MobileApp/app/src/androidTest/java/tr/mobileapp/ExampleInstrumentedTest.java
+++ MobileApp/app/src/androidTest/java/tr/mobileapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package android.mobileapp;
+package tr.mobileapp;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("android.mobileapp", appContext.getPackageName());
+        assertEquals("tr.mobileapp", appContext.getPackageName());
     }
 }
