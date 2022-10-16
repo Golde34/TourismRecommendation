58d06489150b574262eff162cabc5009cd25f975
Long Nguyen <89681530+MadBullz@users.noreply.github.com>
2022-10-16 22:56:17 +0700

    create TripGenerate screen

---
+++ MobileApp/app/src/main/java/tr/mobileapp/Adapter/TripDateAdapter.java
@@ -0,0 +1,77 @@
+package tr.mobileapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import tr.mobileapp.Entity.DayOfTrip;
+import tr.mobileapp.Entity.POIOfDay;
+import tr.mobileapp.R;
+
+public class TripDateAdapter extends RecyclerView.Adapter<TripDateAdapter.ViewHolder>{
+    private Context context;
+    private ArrayList<Date> dates;
+    private LayoutInflater inflater;
+
+    public TripDateAdapter(Context context, ArrayList<DayOfTrip> dayOfTripArrayList) {
+        this.context = context;
+        this.dates = new ArrayList<>();
+        for(int i=0; i<dayOfTripArrayList.size(); i++){
+            dates.add(dayOfTripArrayList.get(i).getDate());
+        }
+        inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public TripDateAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View dayOfTripItem = inflater.inflate(R.layout.day_of_trip_item, parent, false);
+        return new TripDateAdapter.ViewHolder(dayOfTripItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TripDateAdapter.ViewHolder holder, int position) {
+        Date date = dates.get(position);
+        holder.setDate(date);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dates.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvMonth, tvDate, tvDayOfWeek;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bindingView(itemView);
+        }
+
+        private void bindingView(View itemView) {
+            tvMonth = itemView.findViewById(R.id.idTVMonth);
+            tvDate = itemView.findViewById(R.id.idTVDate);
+            tvDayOfWeek = itemView.findViewById(R.id.idTVDayOfWeek);
+        }
+
+        public void setDate(Date date) {
+            DateFormat dayOfWeekFormat = new SimpleDateFormat("EEE");
+            DateFormat monthFormat = new SimpleDateFormat("MMM");
+            DateFormat dateFormat = new SimpleDateFormat("dd");
+
+            tvMonth.setText(monthFormat.format(date));
+            tvDate.setText(dateFormat.format(date));
+            tvDayOfWeek.setText(dayOfWeekFormat.format(date)+".");
+        }
+    }
+}
--- MobileApp/app/src/main/java/tr/mobileapp/MainActivity.java
+++ MobileApp/app/src/main/java/tr/mobileapp/MainActivity.java
@@ -6,12 +6,12 @@ import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -19,15 +19,22 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.Volley;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
+import tr.mobileapp.Activity.TripPlanActivity;
 import tr.mobileapp.Database.AppDatabase;
+import tr.mobileapp.Entity.Account;
+import tr.mobileapp.Entity.Role;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         buttonClick = findViewById(R.id.buttonTest);
         requestQueue = Volley.newRequestQueue(this);
         buttonClick.setOnClickListener((v) -> {
-            Sync("http://10.0.2.2:8080/trip/generate");
+            Sync("http://10.0.2.2:8080/trip/generate", this);
         });
         AppDatabase database = Room.databaseBuilder(this, AppDatabase.class, "Sample.db")
                 .addMigrations(MIGRATION_1_2)
@@ -50,10 +57,32 @@ public class MainActivity extends AppCompatActivity {
         Log.d("DATABASE", "Successful create db: " + database.isOpen());
     }
 
-    public void Sync(String url) {
+    public void Sync(String url, Context context) {
         JSONObject jsonObjectRequest = new JSONObject();
         try {
-            jsonObjectRequest.put("name","anything");
+            JSONObject account = new JSONObject();
+            account.put("id", "1");
+            account.put("address", "gia loc");
+            account.put("DOB", "2001-04-03");
+            account.put("email", "nguyendongducviet2001@gmail.com");
+            account.put("fullname", "Nguyen Viet");
+            account.put("gender", "male");
+            account.put("image", null);
+            account.put("phoneNumber", "0343978156");
+            account.put("level", 0);
+            account.put("levelPoint", 0);
+            account.put("status", 1);
+            account.put("username", "golde");
+            JSONObject role = new JSONObject();
+            role.put("roleId", 1);
+            role.put("roleName", "user");
+            account.put("role", role);
+
+            jsonObjectRequest.put("walletId", account);
+            jsonObjectRequest.put("destination", null);
+            jsonObjectRequest.put("budget", null);
+            jsonObjectRequest.put("startDate", "2022-08-23");
+            jsonObjectRequest.put("endDate", "2022-08-28");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -64,7 +93,10 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("TAG", response.toString() + " i am queen");
+                        Intent i = new Intent(context, TripPlanActivity.class);
+                        i.putExtra("response", response.toString());
+
+                        startActivity(i);
                     }
                 }, new Response.ErrorListener() {
             @Override
