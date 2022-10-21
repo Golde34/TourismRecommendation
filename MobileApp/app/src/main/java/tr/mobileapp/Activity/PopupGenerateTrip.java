561ae0288470d4c954fccefb06461f7e94ff3bb9
Golde34 <nguyendongducviet2001@gmail.com>
2022-10-21 15:28:55 +0700

    need merge
    need merge

---
+++ MobileApp/app/src/main/java/tr/mobileapp/Activity/PopupGenerateTrip.java
@@ -0,0 +1,27 @@
+package tr.mobileapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import tr.mobileapp.R;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class PopupGenerateTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_generate_trip);
+
+    }
+}
---
+++ MobileApp/app/src/main/java/tr/mobileapp/Validate/GenerateTripValidate.java
@@ -0,0 +1,10 @@
+package tr.mobileapp.Validate;
+
+public class GenerateTripValidate {
+    public GenerateTripValidate() {}
+
+    public boolean generateTripValidate(String location, String budget, String startDate, String endDate) {
+        return true;
+        if location ngu =. return false
+    }
+}
