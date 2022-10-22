9d87da8285c537094c70f5976d990c9d58c0b4fc
HoangAnh251220 <anhhhhe140734@fpt.edu.vn>
2022-10-22 12:02:59 +0700

    update

---
+++ MobileApp/app/src/main/java/tr/mobileapp/Activity/HomeRatingAndReviews.java
@@ -0,0 +1,14 @@
+package tr.mobileapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeRatingAndReviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_rating_and_reviews);
+    }
+}
