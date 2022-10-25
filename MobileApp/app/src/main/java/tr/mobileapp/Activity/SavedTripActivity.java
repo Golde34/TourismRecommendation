a02ce9471124f31c925af781306340e55dbc653c
Long Nguyen <89681530+MadBullz@users.noreply.github.com>
2022-10-25 13:06:00 +0700

    Show Detail of Saved Trip done

--- MobileApp/app/src/main/java/tr/mobileapp/Activity/SavedTripActivity.java
+++ MobileApp/app/src/main/java/tr/mobileapp/Activity/SavedTripActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,10 +26,14 @@ import tr.mobileapp.Entity.MyPOI;
 import tr.mobileapp.Entity.POIOfDay;
 import tr.mobileapp.Entity.Tour;
 import tr.mobileapp.R;
+import tr.mobileapp.Ultility.RecyclerItemClickListener;
 
 public class SavedTripActivity extends AppCompatActivity {
 
     private RecyclerView rcvSavedTrip;
+    private ArrayList<Tour> tourArrayList;
+    private Context context = this;
+    JSONArray jsonArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +46,14 @@ public class SavedTripActivity extends AppCompatActivity {
 
         String response = intent.getStringExtra("response");
 
-        JSONArray jsonArr;
-
-        ArrayList<Tour> tourArrayList = new ArrayList<Tour>();
+        tourArrayList = new ArrayList<Tour>();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyy-MM-dd");
         try {
             jsonArr = new JSONArray(response);
             for (int i = 0; i < jsonArr.length(); i++) {
                 JSONObject tourObj = jsonArr.getJSONObject(i);
+
+                JSONArray daysOfTrip = tourObj.getJSONArray("listDays");
 
                 int id = tourObj.getInt("id");
                 Date startDate = new java.sql.Date(dateFormat.parse(tourObj.getString("startDate")).getTime());
@@ -62,9 +70,32 @@ public class SavedTripActivity extends AppCompatActivity {
         TourAdapter adapter = new TourAdapter(this, tourArrayList);
         rcvSavedTrip.setAdapter(adapter);
         rcvSavedTrip.setLayoutManager(new LinearLayoutManager(this));
+
+        rcvSavedTrip.addOnItemTouchListener(new RecyclerItemClickListener(this, rcvSavedTrip, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                try {
+                    JSONObject tourObj = jsonArr.getJSONObject(position);
+
+                    Intent i = new Intent(context, TripPlanActivity.class);
+                    i.putExtra("response", tourObj.toString());
+
+                    startActivity(i);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+        }));
     }
 
     private void bindingView() {
         rcvSavedTrip = findViewById(R.id.idRcvSavedTrip);
     }
+
+
 }
