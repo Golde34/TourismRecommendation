48dd52eb8345f4eea79aa390f61ec3b736ad77f2
quoctkhe150544 <quoctkhe150544@fpt.edu.vn>
2022-10-30 10:41:45 +0700

    commit

--- MobileApp/app/src/main/java/tr/mobileapp/Activity/TripPlanActivity.java
+++ MobileApp/app/src/main/java/tr/mobileapp/Activity/TripPlanActivity.java
@@ -4,11 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,25 +28,31 @@ import org.json.JSONObject;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import tr.mobileapp.Adapter.TripDateAdapter;
 import tr.mobileapp.Adapter.TripDetailRVAdapter;
+import tr.mobileapp.Entity.Account;
 import tr.mobileapp.Entity.DayOfTrip;
 import tr.mobileapp.Entity.MyPOI;
 import tr.mobileapp.Entity.POIOfDay;
 import tr.mobileapp.R;
 import tr.mobileapp.Ultility.RecyclerItemClickListener;
+import tr.mobileapp.VolleySingleton;
 
 public class TripPlanActivity extends AppCompatActivity {
 
     private RecyclerView rcvTripDetail;
     private RecyclerView rcvDayOfTrip;
+    private Button btnHotelView;
     ArrayList<POIOfDay> poiOfDays;
 
     public void bindingView(){
         rcvTripDetail = findViewById(R.id.idRcvTripDetail);
         rcvDayOfTrip = findViewById(R.id.idRcvDayOfTrip);
+        btnHotelView = findViewById(R.id.btnHotelView);
     }
 
     @Override
@@ -125,5 +143,60 @@ public class TripPlanActivity extends AppCompatActivity {
 
             }
         }));
+
+        btnHotelView.setOnClickListener(this::onBtnHotelViewClick);
+    }
+
+
+    private void onBtnHotelViewClick(View view) {
+        Sync("http://10.0.2.2:8080/restingplace/all", this);
+
+
+    }
+
+    public void Sync(String url, Context context) {
+        JSONObject jsonObjectRequest = new JSONObject();
+
+        Log.d("TEST", "-------------");
+        Log.d("TEST", jsonObjectRequest.toString());
+        // Make request for JSONObject
+        JsonArrayRequest jsonObjReq = new JsonArrayRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Intent i = new Intent(context, ViewHotel.class);
+                        i.putExtra("response", response.toString());
+
+                        startActivity(i);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("TAG", "Error: " + error.getMessage());
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+
+        };
+//        Volley.newRequestQueue(this).add(jsonObjReq);
+        VolleySingleton.getmInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
+//        VolleySingleton.getmInstance(getApplicationContext());
+
+    }
+
+    private Map<String, String> setHeaderData() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("header", "Sample header");
+        return headers;
     }
 }
